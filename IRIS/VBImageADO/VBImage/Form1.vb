259c30feb304012864c7178888797b29ccd306e9
wolfman0719 <hsato0719@icloud.com>
2022-03-13 19:42:53 +0900

    Update Form1.vb

--- IRIS/VBImageADO/VBImage/Form1.vb
+++ IRIS/VBImageADO/VBImage/Form1.vb
@@ -7,7 +7,7 @@ Public Class Form1
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         Dim cn As IRISConnection
-        cn = New IRISConnection("Server=127.0.0.1;Port=51773;Namespace=User;Username=_system;Password=SYS")
+        cn = New IRISConnection("Server=127.0.0.1;Port=1972;Namespace=User;Username=_system;Password=SYS")
         cn.Open()
         Dim iris As IRIS
         Dim iriscommand As IRISCommand
@@ -55,7 +55,7 @@ Public Class Form1
         Dim iriscommand As IRISCommand
         Dim memorystream As New MemoryStream
 
-        cn = New IRISConnection("Server=127.0.0.1;Port=51773;Namespace=User;Username=_system;Password=SYS")
+        cn = New IRISConnection("Server=127.0.0.1;Port=1972;Namespace=User;Username=_system;Password=SYS")
         cn.Open()
 
         iris = IRIS.CreateIRIS(cn)
