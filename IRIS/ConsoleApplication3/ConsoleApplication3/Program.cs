6264f7700b6450b108a8cf454b23afe5d4e28c35
wolfman0719 <hsato0719@icloud.com>
2022-03-13 19:51:45 +0900

    Update Program.cs

--- IRIS/ConsoleApplication3/ConsoleApplication3/Program.cs
+++ IRIS/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -14,9 +14,7 @@ namespace ConsoleApplication3
             EventPersister ep = PersisterFactory.CreatePersister();
 
             //ep.Connect("USER", "_SYSTEM", "SYS");
-            //ep.Connect("localhost",1972,"USER", "_SYSTEM", "SYS");
-
-            ep.Connect("localhost", 51773, "USER", "_SYSTEM", "SYS");
+            ep.Connect("localhost",1972,"USER", "_SYSTEM", "SYS");
             Object returnvalue = ep.CallClassMethod("%SYSTEM.Version", "GetVersion");
             Console.Write("ReturnValue = " + returnvalue.ToString());
 
