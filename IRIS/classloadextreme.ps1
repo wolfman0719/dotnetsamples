b265a9efeb1feb0d5e2694d992e6d07b465a2d37
wolfman0719 <hsato0719@icloud.com>
2022-03-13 19:54:53 +0900

    Update classloadextreme.ps1

--- IRIS/classloadextreme.ps1
+++ IRIS/classloadextreme.ps1
@@ -3,7 +3,7 @@ Add-Type -Path 'C:\InterSystems\IRIS\dev\dotnet\bin\v4.5\InterSystems.Data.XEP.d
 $ep = [InterSystems.XEP.PersisterFactory]::CreatePersister()
 
 
-$ep.Connect("localhost", 51773, "USER", "_SYSTEM", "SYS")
+$ep.Connect("localhost", 1972, "USER", "_SYSTEM", "SYS")
 
 
 $ep.CallClassMethod("%SYSTEM.OBJ","Load","c:\temp\load.xml","ck-d")
