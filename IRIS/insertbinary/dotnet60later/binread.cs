333d5e43c5742e6bedba6ccc1c1c8359197b521e
wolfman0719 <hsato0719@icloud.com>
2025-10-09 15:14:37 +0900

    Add files via upload

---
+++ IRIS/insertbinary/dotnet60later/binread.cs
@@ -0,0 +1,57 @@
+﻿// binread.cs
+
+using System;
+using System.IO;
+using System.Data;
+using InterSystems.Data.IRISClient;
+using InterSystems.Data.IRISClient.ADO;
+
+namespace binaryfileread {
+
+    class binaryfileread 
+    {
+    [STAThread]
+    static void Main(string[] args) {
+
+        IRISCommand spIRIS;
+        IRISConnection cnIRIS;
+        IRISTransaction txIRIS = null;
+
+        //存在するファイルを指定する
+        FileStream fs = new FileStream(
+        @"/Users/hsatoctr/work/test.jpeg", FileMode.Open, FileAccess.Read);
+
+        int fileSize = (int)fs.Length; // ファイルのサイズ
+        byte[] buf = new byte[fileSize]; // データ格納用配列
+
+        long readSize; // Readメソッドで読み込んだバイト数
+        int remain = fileSize; // 読み込むべき残りのバイト数
+
+        readSize = fs.Read(buf, 0, (int)fs.Length);
+
+        string IRISConnectString = "Server = localhost;Port=1972;Namespace=User;Password=SYS;User ID = _SYSTEM;";
+        cnIRIS = new IRISConnection(IRISConnectString);
+        cnIRIS.Open();
+        spIRIS = new IRISCommand("Insert into MyApp.Person2(Name, Picture) Values(?, ?)", cnIRIS, txIRIS);
+
+        IRISParameter pName = new IRISParameter();
+        pName.ParameterName = "Name";
+        pName.IRISDbType = IRISDbType.NVarChar;
+        pName.Direction = ParameterDirection.Input;
+        pName.Value = "Hoge Hoge";
+        spIRIS.Parameters.Add(pName);
+
+        IRISParameter pPicture = new IRISParameter();
+        pPicture.ParameterName = "Picture";
+        pPicture.IRISDbType = IRISDbType.LongVarBinary;
+        pPicture.Direction = ParameterDirection.Input;
+        pPicture.Value = buf;
+        spIRIS.Parameters.Add(pPicture);
+
+        spIRIS.ExecuteNonQuery();
+
+        fs.Dispose();
+        cnIRIS.Close();
+    }
+   }
+}
