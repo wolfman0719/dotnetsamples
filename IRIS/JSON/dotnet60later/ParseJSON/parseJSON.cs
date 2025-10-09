a387cda193e4f109f12279d98a05b740aa2e7ef2
wolfman0719 <hsato0719@icloud.com>
2025-10-09 14:56:44 +0900

    Add files via upload

---
+++ IRIS/JSON/dotnet60later/ParseJSON/parseJSON.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Web;
+
+namespace JsonTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // URLエンコーディング
+            string url = "http://localhost:8080/api/dotnetrest/getcolors";
+            // HTTPアクセス
+            var req = WebRequest.Create(url);
+            req.Headers.Add("Accept-Language:ja,en-us;q=0.7,en;q=0.3");
+            var res = req.GetResponse();
+            // レスポンス(JSON)をオブジェクトに変換
+            ServiceResult info;
+            using (res)
+            {
+                using (var resStream = res.GetResponseStream())
+                {
+                    var serializer = new DataContractJsonSerializer(typeof(ServiceResult));
+                    info = (ServiceResult)serializer.ReadObject(resStream);
+                }
+            }
+            // 結果を出力
+            Debug.WriteLine("red: " + info.red);
+            Debug.WriteLine("blue: " + info.blue);
+            Debug.WriteLine("yellow: " + info.yellow);
+        }
+    }
+
+    [DataContract]
+    public class ServiceResult
+    {
+        [DataMember]
+        public string red { get; set; }
+        [DataMember]
+        public string blue { get; set; }
+        [DataMember]
+        public string yellow { get; set; }
+    }
+}
