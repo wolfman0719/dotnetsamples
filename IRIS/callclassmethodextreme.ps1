d78c08b3973869d3825a645b83be9c8ea3f7e902
wolfman0719 <hsato0719@icloud.com>
2025-10-10 22:47:30 +0900

    Delete IRIS/callclassmethodextreme.ps1

--- IRIS/callclassmethodextreme.ps1
+++
@@ -1,9 +0,0 @@
-Add-Type -Path 'C:\InterSystems\IRIS\dev\dotnet\bin\v4.5\InterSystems.Data.XEP.dll'
-
-$ep = [InterSystems.XEP.PersisterFactory]::CreatePersister()
-
-
-$ep.Connect("localhost", 1972, "USER", "_SYSTEM", "SYS")
-
-
-$ep.CallClassMethod("%SYSTEM.Version","GetVersion")
