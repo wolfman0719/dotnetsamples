e14a3db097a435965bc59a4b44cd666c4ebc8c33
wolfman0719 <hsato0719@icloud.com>
2025-10-10 22:56:17 +0900

    Add files via upload

---
+++ IRIS/VBImageADO/60later/Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(285, 43)
+        Button1.Margin = New Padding(4, 5, 4, 5)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(143, 60)
+        Button1.TabIndex = 0
+        Button1.Text = "Upload!"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(285, 138)
+        Button2.Margin = New Padding(4, 5, 4, 5)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(143, 63)
+        Button2.TabIndex = 1
+        Button2.Text = "Display!"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(63, 237)
+        PictureBox1.Margin = New Padding(4, 5, 4, 5)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(365, 350)
+        PictureBox1.TabIndex = 2
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(475, 607)
+        Controls.Add(PictureBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Margin = New Padding(4, 5, 4, 5)
+        Name = "Form1"
+        Text = "Test"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
